1ba480b40d733d0e2bba39e0abc4c00f8de8e0a6
Carlos Mañas <carlos@spacenomads.com>
2019-05-23 13:58:59 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {fetchPokemons} from './services/fetchPokemons';
+import Home from './components/Home';
+import PokemonDetail from './components/PokemonDetail';
+import {Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemons: [],
+      filterName: ''
+    };
+    this.handleNameFilter = this.handleNameFilter.bind(this);
+    this.resetFilters = this.resetFilters.bind(this);
+  }
+
+  componentDidMount(){
+    this.getPokemons();
+  }
+
+  resetFilters() {
+    this.setState({
+      filterName: ''
+    });
+  }
+
+  getPokemons(){
+    fetchPokemons()
+      .then(data => {
+
+        const newData = data.map((item, index) => {
+          return {...item, id: `pokeid-${index}`};
+        });
+
+        this.setState({
+          pokemons: newData 
+        })
+      });
+  }
+
+  handleNameFilter(event) {
+    const query = event.currentTarget.value;
+
+    this.setState({
+      filterName: query
+    })
+  }
+
+  render() {
+    const {pokemons, filterName} = this.state;
+    return (
+      <div className="app">
+        <Switch>
+          <Route exact path="/" render={() => <Home
+            pokemons={pokemons} 
+            handleNameFilter={this.handleNameFilter}
+            filterName={filterName}
+            />
+          } />
+          <Route path="/detail/:criatura" render={(recogeParametros) => <PokemonDetail parametros={recogeParametros} pokemons={pokemons} resetFilters={this.resetFilters} />} />
+        </Switch>
+
+        
+        
+      </div>
+    );
+  }
 }
 
 export default App;
